fff9f56af6fbc88cdb85fb7d670d4a7b653d97b9
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-06-21 23:16:17 +0100

    Feat: add editExpense route

--- server/Controllers/expenseController.ts
+++ server/Controllers/expenseController.ts
@@ -17,15 +17,11 @@ declare module 'express-serve-static-core' {
 export const getAllExpenses = async(req: Request, res: Response): Promise<Response> => {
     try {
         const userId = req.decodedToken
-        if (!userId) {
-            return res.status(401).json({message: "Invalid Credentials"})
-        }
-        
         const getAllUsersExpenses = await Expense.find({user: userId})
         if(!getAllUsersExpenses){
             return res.status(404).json({message: "No Expenses record"})
         }
-        console.log(getAllUsersExpenses);
+        console.log("this user expeses",getAllUsersExpenses);
         
         return res.status(200).json(getAllUsersExpenses)
     } catch (error) {
@@ -37,7 +33,6 @@ export const getAllExpenses = async(req: Request, res: Response): Promise<Respon
 export const createExpense = async(req: Request, res: Response): Promise<Response> => {
     try {
         const userId = req.decodedToken
-        console.log(userId);
         
         const {title, amount, category}: ExpenseType = req.body
         if (!title || !amount || !category) {
@@ -54,9 +49,48 @@ export const createExpense = async(req: Request, res: Response): Promise<Respons
             user: user._id
         })
         await newExpense.save().then(console.log).catch(console.error)
+        user.expenses.push(newExpense._id)
+        await user.save()
         return res.status(201).json(newExpense)
     } catch (error) {
         console.log(error)
         return res.status(500).json({message: "Error creating expense"})
+    }
+}
+
+export const editExpense = async(req: Request, res: Response): Promise<Response> => {
+    try {
+        const { Id } = req.params as {Id: string}
+        const { title, amount, category }: ExpenseType = req.body
+        const userId = req.decodedToken
+        const user = await User.findById(userId)
+        if (!user) {
+            return res.status(404).json({message: 'User Not Found'})
+        }
+        const checkExpense = await Expense.findById(Id)
+        if (!checkExpense) {
+            return res.status(404).json({message: 'Expense Not Found'})
+        }
+        if (checkExpense.user.toString() !== userId) {
+            return res.status(401).json({message: "Unauthorized"})
+        }
+        const newExpense: Partial<ExpenseType> = {}
+        if (title) newExpense.title = title
+        if (amount) newExpense.amount = amount
+        if (category) newExpense.category = category
+        const updatedExpense = await Expense.findByIdAndUpdate(Id, newExpense, {new: true})
+       return res.status(200).json(updatedExpense) 
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Error Editing Expenses"})
+    }
+}
+
+export const deleteExpense = async(_req: Request, res: Response): Promise<Response> => {
+    try {
+       return res.send('Delete expense') 
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Error Deleting Expenses"})
     }
 }
--- server/Routes/expense.route.ts
+++ server/Routes/expense.route.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 export const expenseRouter = express.Router()
 import { asyncHandler } from '../Utils/helper'
-import { createExpense, getAllExpenses } from '../Controllers/expenseController'
+import { createExpense, getAllExpenses, editExpense, deleteExpense } from '../Controllers/expenseController'
 import { decodeToken } from '../Utils/token'
 import { getToken } from '../Middleware/getToken'
+
 const auth = [getToken, decodeToken]
+
 expenseRouter.route('/').get(...auth, asyncHandler(getAllExpenses)).post(...auth, asyncHandler(createExpense))
+expenseRouter.route('/:Id').patch(...auth, asyncHandler(editExpense)).delete(...auth, asyncHandler(deleteExpense))
