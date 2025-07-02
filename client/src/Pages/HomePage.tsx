fcc379a9dc6eb901ebff79203e0b4359e62c642b
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-07-02 01:10:19 +0100

    Feat: add expense Store

--- client/src/Pages/HomePage.tsx
+++ client/src/Pages/HomePage.tsx
@@ -1,9 +1,26 @@
 import MainContent from "@/components/MainContent"
 import SideBar from "@/components/SideBar"
+// import { useNavigate } from "react-router-dom"
+import { Button } from "@/components/ui/button"
+import { useEffect } from "react"
+import { useExpenseStore } from "@/Store/Expense/expenseStore"
+
 const HomePage = () => {
+  // const navigate = useNavigate()
+  const {getAllExpenses, expenses, authUser} = useExpenseStore()
+  useEffect(()=>{
+    if (authUser) {
+      getAllExpenses()
+    }
+  }, [getAllExpenses, authUser])
+  console.log(expenses)
+  const handleLogout = () => {
+    localStorage.removeItem('authUser')
+    
+  }
   return (
     <>
-      <div className="w-full h-[10%] text-3xl text-center font-bold p-4 bg-amber-300">Expense Tracker</div>
+      <div className="w-full h-[10%] text-3xl font-bold p-4 flex justify-between items-center bg-amber-300">Expense Tracker <Button className="cursor-pointer" variant="destructive" onClick={handleLogout}>Logout</Button></div>
     <div className="flex gap-[2%] flex-wrap content-start px-4 py-7 bg-slate-200 h-screen">
       <MainContent/>
       <SideBar/>
--- client/src/Store/axiosInstance.tsx
+++ client/src/Store/axiosInstance.tsx
@@ -1,6 +1,20 @@
 import axios from "axios"
 
+
 export const axiosInstance = axios.create({
     baseURL: 'http://localhost:4000/api',
     withCredentials: true
 })
+
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+      const getUser = localStorage.getItem('authUser')
+      const user = getUser ? JSON.parse(getUser) : null
+      if (user?.token) {
+        config.headers.Authorization = `Bearer ${user.token}`
+      }
+      return config
+    },
+    (error) => Promise.reject(error)
+  )
