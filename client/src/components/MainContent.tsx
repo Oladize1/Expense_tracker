a8d24bf27a17fcccdb2baa898b3f04149eda2b07
Pamilerin Marcus Oladunjoye <pamoladize10@gmail.com>
2025-07-20 02:36:50 +0100

    Fix: create Expense route to update amount of the same category expenses

--- client/src/components/MainContent.tsx
+++ client/src/components/MainContent.tsx
@@ -5,21 +5,22 @@ import { useExpenseStore } from "@/Store/Expense/expenseStore";
 import { ChartBarLabel } from "./Chart";
 
 type Result = {
-  Investment: string,
-  Savings: string,
-  Expense: string
+  Investment: number,
+  Savings: number,
+  Expense: number
 }
 
 const MainContent = () => {
   const { expenses } = useExpenseStore();
   console.log("expense", expenses);
-  const result: Result = {}
+  const result: Result = {
+    Savings: 0,
+    Investment: 0,
+    Expense: 0
+  }
 
   expenses.forEach(expense=>{
     const category = expense.category
-    if (!result[category]) {
-      result[category] = {}
-    }
     result[category] = expense.amount
   })
 console.log(result);
@@ -27,9 +28,9 @@ console.log(result);
   return (
     <div className="grow md:h-3/4">
       {/* Scrollable Container on Small Screens */}
-      <div className="flex items-stretch gap-3 overflow-x-auto md:overflow-hidden px-2 py-2 snap-x snap-mandatory">
+      <div className="flex gap-3 w-full px-2 py-2">
         {/* Total Expense Card */}
-        <div className="rounded-2xl flex items-center bg-gradient-to-r from-emerald-400 to-cyan-400 flex-shrink-0 w-[250px] md:w-1/3 justify-between px-4 py-4 snap-center">
+        <div className="rounded-2xl flex items-center bg-gradient-to-r from-emerald-400 to-cyan-400 w-1/3 justify-between px-2 py-4">
           <div className="flex flex-col">
             <h2 className="text-gray-100">Total Expense</h2>
             <p className="text-3xl font-bold text-white">{`$ ${result.Expense}`}</p>
@@ -38,7 +39,7 @@ console.log(result);
         </div>
 
         {/* Investment Card */}
-        <div className="rounded-2xl flex items-center bg-gradient-to-r from-emerald-400 to-cyan-400 flex-shrink-0 w-[250px] md:w-1/3 justify-between px-4 py-4 snap-center">
+        <div className="rounded-2xl flex items-center bg-gradient-to-r from-emerald-400 to-cyan-400  w-1/3 justify-between px-2 py-4">
           <div className="flex flex-col">
             <h2 className="text-gray-200">Investment</h2>
             <p className="text-3xl font-bold text-white">{`$ ${result.Investment}`}</p>
@@ -47,7 +48,7 @@ console.log(result);
         </div>
 
         {/* Savings Card */}
-        <div className="rounded-2xl flex items-center bg-gradient-to-r from-violet-500 to-purple-500 flex-shrink-0 w-[250px] md:w-1/3 justify-between px-4 py-4 snap-center">
+        <div className="rounded-2xl flex items-center bg-gradient-to-r from-violet-500 to-purple-500 w-1/3 justify-between px-2 py-4">
           <div className="flex flex-col">
             <h2 className="text-gray-200">Savings</h2>
             <p className="text-3xl font-bold text-white">$ {result.Savings}</p>
